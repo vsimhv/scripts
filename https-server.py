0691ce91c4e159b8b4dd786a87cf28f7c02733bd
Paweł <pawel.bartunek@gmail.com>
2018-05-25 18:54:17 +0200

    added python https server

---
+++ https-server.py
@@ -0,0 +1,22 @@
+# generate server.pem:
+#    openssl req -new -x509 -keyout server.pem -out server.pem -days 365 -nodes
+# run as follows:
+#    python https-server.py
+
+import SimpleHTTPServer
+import SocketServer
+import logging
+import ssl
+
+PORT = 443
+
+class GetHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
+
+    def do_GET(self):
+        logging.error(self.headers)
+        SimpleHTTPServer.SimpleHTTPRequestHandler.do_GET(self)
+
+Handler = GetHandler
+httpd = SocketServer.TCPServer(("", PORT), Handler)
+httpd.socket = ssl.wrap_socket (httpd.socket, certfile='./server.pem', server_side=True)
+httpd.serve_forever()

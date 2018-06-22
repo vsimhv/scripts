ee3efa37eb48d6d470c2dc9207021ac1aaafb830
Pawel <pawel.bartunek@gmail.com>
2018-06-22 16:48:48 +0200

    port and service name search

--- nmap-parser.rb
+++ nmap-parser.rb
@@ -10,68 +10,49 @@ class NmapParser
     @options = options
     @nmapXml = Nmap::XML.new(options[:input]) 
 
-    if (@options[:verbose])
-      info
-    end
-
-    case
-    when @options[:search]
-      search_services(@options[:search])
-    when @options[:port]
-      search_ports(@options[:port])
-    else
-      opened_ports
-    end
+    print_info if (@options[:verbose])
+    search_services
   end
 
-  def info
-    print "Scan date: "
+  def print_info
+    print "Scan".ljust(10)
     puts @nmapXml.run_stats
-    print "Scan type: "
+    print "Type:".ljust(10)
     puts @nmapXml.scan_info
+    print "Command:".ljust(10)
+    puts @nmapXml.scanner.arguments
+    print "Version:".ljust(10)
+    puts @nmapXml.scanner.version
   end
 
-  def opened_ports
-    puts "\nOpened ports" if (@options[:verbose])
+  def search_services
+    print_search_summary if (@options[:verbose])
 
     @nmapXml.each_up_host do |host|
       host.each_open_port do |port|
-        print "#{host.ip}:#{port.number}\t#{port.service}"
-        print "\t#{port.service.product}" unless port.service.nil?
-        puts
-      end
-    end
-  end
-
-  def search_services(search)
-    puts "\nHosts with services matching search: #{search}" if (@options[:verbose])
-
-    @nmapXml.each_up_host do |host|
-      host.each_open_port do |port|
-        if (not port.service.nil? and port.service.name.include?(search))
-          print "#{host.ip}:#{port.number}\t#{port.service}"
-          print "\t#{port.service.product}" unless port.service.nil?
-          print "\t#{port.service.version}" unless port.service.nil?
-          puts
+        if (not port.service.nil? and 
+            (@options[:service].nil? or port.service.name.include?(@options[:service])) and
+            (@options[:port].nil? or port.number == @options[:port])
+           )
+          print_service(host, port)
         end
       end
     end
   end
 
-  def search_ports(search)
-    puts "\nHosts with opened port #{search}" if (@options[:verbose])
-
-    @nmapXml.each_up_host do |host|
-      host.each_open_port do |port|
-        if (port.number == search)
-          print "#{host.ip}:#{port.number}\t#{port.service}"
-          print "\t#{port.service.product}" unless port.service.nil?
-          puts
-        end
-      end
-    end
+  def print_search_summary
+    print "\nHosts with "
+    print "port: #{@options[:port]} opened" if @options[:port]
+    print " and " if @options[:port] and @options[:service]
+    puts "service name: '#{@options[:service]}'" if @options[:service]
   end
 
+  def print_service(host, port)
+    print "#{host.ip}:#{port.number}"
+    print "\t#{port.service.name}"
+    print "\t#{port.service.product}"
+    puts "\t#{port.service.version}"
+  end
 end
 
 class NmapParserOptions
@@ -83,13 +64,16 @@ class NmapParserOptions
       opts.on("-v", "--verbose") do
         options[:verbose] = true
       end
-      opts.on("-f", "--file NMAP_XML", String, "Nmap XML output file") do |val|
+      opts.on("-f", "--file NMAP_XML", String,
+              "Nmap XML output file") do |val|
         options[:input] = val
       end
-      opts.on("-s", "--search SEARCH", String, "Search for services with specified name") do |val|
-        options[:search] = val
+      opts.on("-s", "--service NAME", String,
+              "Search for services with specified name") do |val|
+        options[:service] = val
       end
-      opts.on("-p", "--port NUMBER", Integer, "Search for services with specified port opened") do |val|
+      opts.on("-p", "--port NUMBER", Integer,
+              "Search for services with specified port") do |val|
         options[:port] = val
       end
     end.parse!

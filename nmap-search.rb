0939c3e5a9775a466e3c94c3e634fa60c4f1272e
Pawel <pawel.bartunek@gmail.com>
2018-06-28 16:16:43 +0200

    refactoring

--- nmap-search.rb
+++ nmap-search.rb
@@ -2,7 +2,48 @@
 require 'nmap/xml'
 require 'optparse'
 
-class NmapParser
+class NmapSearch
+
+  class Options
+    def self.parse(args)
+      options = {}
+      OptionParser.new do |opts|
+        opts.banner = "Usage: #{File.basename($0)} -f NMAP_OUTPUT.xml [OPTIONS]"
+        opts.on("-v", "--verbose") do
+          options[:verbose] = true
+        end
+        opts.on("-f", "--file NMAP_XML", String,
+                "Nmap XML output file") do |val|
+          options[:input] = val
+        end
+        opts.on("-s", "--service NAME", String,
+                "Search for services with specified name") do |val|
+          options[:service] = val
+        end
+        opts.on("-p", "--port NUMBER", Integer,
+                "Search for services with specified port") do |val|
+          options[:port] = val
+        end
+        opts.on("-i", "--ip IP", String,
+                "Search for hosts with specified ip address") do |val|
+          options[:ip] = val
+        end
+        opts.on("-P", "--product [NAME]",
+                "Search for ports with discovered product name") do |val|
+          options[:product] = val.nil? ? "" : val
+        end
+      end.parse!
+
+      if (options[:input].nil?)
+        raise "Nmap XML file not specified"
+      end
+
+      unless (File.exist?(options[:input]))
+        raise "Nmap XML file '#{options[:input]}' does not exist!"
+      end
+      options
+    end
+  end
 
   class Matcher
     def self.ip(ip, host)
@@ -28,9 +69,9 @@ class NmapParser
   @nmapXml = nil
   @options = nil
 
-  def initialize(options)
-    @options = options
-    @nmapXml = Nmap::XML.new(options[:input]) 
+  def initialize(argv)
+    @options = Options.parse(argv)
+    @nmapXml = Nmap::XML.new(@options[:input]) 
 
     print_info if (@options[:verbose])
     search_services
@@ -68,61 +109,19 @@ class NmapParser
     print "\nHosts with "
     print "port: #{@options[:port]} opened" if @options[:port]
     print " and " if @options[:port] and @options[:service]
-    puts "service name: '#{@options[:service]}'" if @options[:service]
+    puts  "service name: '#{@options[:service]}'" if @options[:service]
   end
 
   def print_service(host, port)
     print "#{host.ip}:#{port.number}"
     print "\t#{port.service.name}"
     print "\t#{port.service.product}"
-    puts "\t#{port.service.version}"
-  end
-end
-
-class NmapParserOptions
-
-  def self.parse(args)
-    options = {}
-    OptionParser.new do |opts|
-      opts.banner = "Usage: #{File.basename($0)} -f NMAP_XML"
-      opts.on("-v", "--verbose") do
-        options[:verbose] = true
-      end
-      opts.on("-P", "--product [NAME]", "Show only ports with discovered product name") do |val|
-        options[:product] = val.nil? ? "" : val
-      end
-      opts.on("-f", "--file NMAP_XML", String,
-              "Nmap XML output file") do |val|
-        options[:input] = val
-      end
-      opts.on("-s", "--service NAME", String,
-              "Search for services with specified name") do |val|
-        options[:service] = val
-      end
-      opts.on("-p", "--port NUMBER", Integer,
-              "Search for services with specified port") do |val|
-        options[:port] = val
-      end
-      opts.on("-i", "--ip IP", String,
-              "Search for hosts with specified ip address") do |val|
-        options[:ip] = val
-      end
-    end.parse!
-
-    if (options[:input].nil?)
-      raise "Nmap XML file not specified"
-    end
-
-    unless (File.exist?(options[:input]))
-      raise "Nmap XML file '#{options[:input]}' does not exist!"
-    end
-    options
+    puts  "\t#{port.service.version}"
   end
 end
 
 begin
-  options = NmapParserOptions.parse(ARGV)
-  NmapParser.new(options)
+  NmapSearch.new(ARGV)
 rescue Exception => e
   puts e
 end
